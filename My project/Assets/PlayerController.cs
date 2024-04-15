1a25cf64e8f3be6d9181eba0aa642b4bce2e58b0
Phuong Lan <52100812@student.tdtu.edu.vn>
2024-04-15 23:30:35 +0700

    Lab 7

--- My project/Assets/PlayerController.cs
+++ My project/Assets/PlayerController.cs
@@ -1,37 +1,39 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    /*
-    public float verticalValue;
-    private Animator animator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-      
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+  
+        public float verticalValue;
+        private Animator animator;
+        // Start is called before the first frame update
+        void Start()
         {
-            animator.SetFloat("Speed", 0.6F);
+            animator = GetComponent<Animator>();
+
+
         }
-        if (Input.GetKeyDown(KeyCode.RightShift))
+
+        // Update is called once per frame
+        void Update()
         {
-            animator.SetFloat("Speed", 1.5F);
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                animator.SetFloat("Speed", 0.6F);
+            }
+            if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                animator.SetFloat("Speed", 1.5F);
+            }
         }
-    }
-    private void Awake()
-    {
-        verticalValue = transform.position.x;
-        animator = GetComponent<Animator>();
-        animator.SetFloat("Speed", 0.25F);
-    }
-    */
+        private void Awake()
+        {
+            verticalValue = transform.position.x;
+            animator = GetComponent<Animator>();
+            animator.SetFloat("Speed", 0.25F);
+        }
+    
+
+
 }
